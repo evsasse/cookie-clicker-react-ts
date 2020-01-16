418b9d0263f6dadac77e303eb7178ae65de948f2
evsasse <evsasse@gmail.com>
2020-01-16 09:34:01 -0300

    adding grandmas

--- src/StoreColumn/index.js
+++ src/StoreColumn/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 
-function StoreColumn({ cookies,  cursors, buy }) {
+function StoreColumn({ cookies, grandmas, cursors, buy }) {
   const cursorPrice = parseInt(15 * (1.2 ** cursors));
   const cursorUnavailable = cookies < cursorPrice;
+
+  const grandmaPrice = parseInt(100 * (1.2 ** grandmas));
+  const grandmaUnavailable = cookies < grandmaPrice;
 
   return (
     <div>
       <button
-        className="btn btn-outline-dark w-100"
+        className="btn btn-outline-dark mb-3 w-100"
         disabled={cursorUnavailable}
         onClick={() => buy('cursors', cursorPrice)}
       >
@@ -20,6 +23,25 @@ function StoreColumn({ cookies,  cursors, buy }) {
             Buy cursor for <strong>{ cursorPrice }</strong><br />
             <span className="small text-muted">
               Current level: { cursors }
+            </span>
+          </div>
+        </div>
+      </button>
+
+      <button
+        className="btn btn-outline-dark mb-3 w-100"
+        disabled={grandmaUnavailable}
+        onClick={() => buy('grandmas', grandmaPrice)}
+      >
+        <div class="row no-gutters align-items-center">
+          <div class="col-2">
+            <i class="fas fa-heart mr-2"></i>
+          </div>
+
+          <div class="col-10 text-left">
+            Buy grandma for <strong>{ grandmaPrice }</strong><br />
+            <span className="small text-muted">
+              Current level: { grandmas }
             </span>
           </div>
         </div>
